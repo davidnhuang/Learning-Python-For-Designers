02a47db2988c92b5b7e41a336dd121d7bfed24fd
David <David.N.Huang@gmail.com>
2018-03-16 19:15:35 -0700

    Added First Assignment
    First assignment to practice using classes: determining whether an integer is even or odd

---
+++ Classes/Reverse_String_With_Classes.py
@@ -0,0 +1,12 @@
+# Author: David Huang
+# Learning Python for Designers
+# Reverse String with Classes
+# 3 - 16 - 2018
+
+# Even or Odd: determine whether a user inputted string is an even or an odd integer; handle all processing within one
+# class / object
+
+class even_or_odd():
+
+    def __init__(self, user_input_string): # describes what user string is
+        self.user_string = user_input_string # tells the class that the string is based on user input
