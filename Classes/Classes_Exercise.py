be6a9c153a0c7bcb346ab957ee32827a79d48f8e
David <David.N.Huang@gmail.com>
2018-03-18 23:54:03 -0700

    Finished third example of class
    created the life class with sunflower as an instance; added more notes

--- Classes/Classes_Exercise.py
+++ Classes/Classes_Exercise.py
@@ -24,11 +24,11 @@ class dog(): # creates the class dog, which produces a dog with the attribute co
 
 tony_the_bulldog = dog('black', 'Tony') # here, we are declaring Tony the Bulldog as a dog with the name Tony and is Black
 
-tony_the_bulldog.dog_attribute() # since before, we assigned tony_the_bulldog to dog class, here, we see tony_the_bulldog
+#tony_the_bulldog.dog_attribute() # since before, we assigned tony_the_bulldog to dog class, here, we see tony_the_bulldog
                                  # being passed into the self position. Therefore, the attribute black and Tony are also
                                  # being passed and called as well
-tony_the_bulldog.bark() # barks
-tony_the_bulldog.howl() # howls
+#tony_the_bulldog.bark() # barks
+#tony_the_bulldog.howl() # howls
 
 # A clear distinction is that a class is NOT a function; it is a series of methods that help define an object.
 # A dog has four legs, a color, and can bark and howl. This means that the methods within the class would define those.
@@ -52,13 +52,48 @@ class mammal():
         else:
             print ('dunno')
 
-cow = mammal('cow','grass', 'domestic', 'is not')
+#cow = mammal('cow','grass', 'domestic', 'is not')
 
-cow.description()
-cow.cow_noise()
+#cow.description()
+#cow.cow_noise()
 
 class life():
 
-    def __init__(self, name, photosynthetic, predatory, sexual_reproduction):
-        self.name = name
-        self.photosynthetic = photosynthetic
+    def __init__(self, name, energy_source, predatory, reproduction): # this initializes the class
+        self.name = name # all life has a name
+        self.energy_source = energy_source # describes where the life gets it's energy input
+        self.predatory = predatory # describes whether its a predatory organism or not
+        self.reproduction = reproduction # describes the reproduction process
+
+    def describe(self): # this method calls all attributes fo the life
+        print (self.name, 'gets its energy from', self.energy_source, ', it is', self.predatory, 'and reproduces',
+               self.reproduction)
+
+    def name(self):
+        print (self.name)
+
+    def movement(self):
+        if self.predatory == 'a predator':
+            return True
+        elif self.predatory == 'not a predator':
+            return False
+
+    def plant(self):
+        if self.energy_source == 'photosynthesis':
+            return True
+
+# to use the class, define the life first, and then call the attributes
+sunflower = life('sunflower', 'photosynthesis', 'not a predator', 'asexually') # here, we created sunflower
+# as an instance of the class Life, we've described it with the required attributes
+
+sunflower.describe() # let's describe what a sunflower is based on our inputs
+
+print ('can a ', sunflower.name, 'move?', end='  ')
+print (sunflower.movement())
+
+print ('is', sunflower.name, 'a plant?', end='  ')
+print (sunflower.plant())
+
+# for a class to work, we must start by creating the instance just like we did in line 86
+# at that point, we created sunflower as an instance of the class, which tells the program to read sunflower as self, and
+# create a class with the specific attributes
