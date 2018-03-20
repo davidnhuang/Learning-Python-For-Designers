0c5f58952eb9d96a6c3ba79b2570aa0012d49748
David <David.N.Huang@gmail.com>
2018-03-19 20:58:19 -0700

    Starting CSGO V2
    starting to redo CSGO simulation with object oriented programming

---
+++ Classes/CSGO_Stochastic_Sim_V2.py
@@ -0,0 +1,33 @@
+# David Huang
+# Learning Python for Designers
+# CSGO Simulation V2
+# March 2018
+
+# IMPORTS
+import random
+import time
+
+# CONSTANTS
+# Team Data
+# Dataset format: [team name, t / ct side, team score, living player count, ct elimination rate, defuse success rate,
+# bomb plant success rate]
+Team_A_Dataset = ['Team NiP', 'None', 0, 5, 48.2, 55, 33]
+Team_B_Dataset = ['Team Fnatic', 'None', 0, 5, 48.1, 50, 35]
+# Game data
+# Dataset format: [total rounds played, bomb planted, bomb defused, bomb detonated]
+Game_Data = [0, False, False, False]
+
+# VARIABLES
+
+# CLASSES
+class Team():
+
+    def __init__(self, team_data):
+
+class Game():
+
+    def __init__(self, Game_Data):
+
+# FUNCTIONS
+
+# MAIN
