e8dc97b44a028b24f0821139ab57bf88bc66e339
David <David.N.Huang@gmail.com>
2018-03-19 21:06:04 -0700

    Merged Datasets
    Merged team A and team B data into a single master dataset; beging constructing classes

--- Classes/CSGO_Stochastic_Sim_V2.py
+++ Classes/CSGO_Stochastic_Sim_V2.py
@@ -9,25 +9,31 @@ import time
 
 # CONSTANTS
 # Team Data
-# Dataset format: [team name, t / ct side, team score, living player count, ct elimination rate, defuse success rate,
-# bomb plant success rate]
-Team_A_Dataset = ['Team NiP', 'None', 0, 5, 48.2, 55, 33]
-Team_B_Dataset = ['Team Fnatic', 'None', 0, 5, 48.1, 50, 35]
+# Dataset format: [team A name, team A side, team B name, team B side, team A score, team B score, team A alive player
+# count, team B alive player count, team A ct elimination rate, team B ct elimination rate, team A bomb plant success
+# rate, team B bomb plant success rate, team A bomb diffuse success rate, team B bomb deffuse success rate]
+Team_Dataset = ['Team NiP', 'None', 'Team Fnatic', 'None', 0, 0, 5, 5, 48.2, 48.1, 55, 50, 33, 35]
+
 # Game data
 # Dataset format: [total rounds played, bomb planted, bomb defused, bomb detonated]
 Game_Data = [0, False, False, False]
 
 # VARIABLES
 
+
 # CLASSES
 class Team():
 
-    def __init__(self, team_data):
+    def __init__(self, Team_Data):
+        self.Team_Data = Team_Data
 
 class Game():
 
     def __init__(self, Game_Data):
+        self.Game_Data = Game_Data
 
+    def ct_win_msg(self):
+        
 # FUNCTIONS
 
 # MAIN
