c800847af13183ea1aaa2708b932729ffd7401d5
David <David.N.Huang@gmail.com>
2018-03-23 22:28:30 -0700

    Added second method to outcome class
    Added methods that determine winners of the game based on dataset

--- Classes/CSGO_Stochastic_Sim_V2.py
+++ Classes/CSGO_Stochastic_Sim_V2.py
@@ -22,25 +22,31 @@ Game_Data = [0, False, False, False, False, False]
 
 
 # CLASSES
-class Finality(): # this class handles all the possible endings for the game, which are ties, team A win, team B win
+class game_outcome(): # this class handles all the possible endings for the game, which are ties, team A win, team B win
 
-    def __init__(self, Game_Data, Team_Data): # using Game_Data dataset to handle finalities
-        self.Game_Data = Game_Data
-        self.Team_Data = Team_Data
+    def __init__(self, Game_Data, Team_Data): # using Game_Data dataset to handle game outcomes
+        self.Game_Data = Game_Data # this allows the class to import the datasets from Game Data
+        self.Team_Data = Team_Data # this allows the class to import the datasets from Team Data
 
     def game_tie(self): # this method determines whether the game is a tie or not
         # A tie happens when the max amount of rounds (30) is played and both teams reach 15 round won
         if self.Team_Data[4] == 15 and self.Team_Data[5] == 15 and self.Game_Data[0] == 30:
             print ('Game Tie') # declare a tie
 
+    def game_winner(self): # this method determines whether there is a victor in the game and print their victory prompt
+        if self.Team_Data[4] == 16: # A team wins the game if they reach 16 rounds won
+            print (self.Team_Data[0], 'clinched the game with 16 rounds won')
+        elif self.Team_Data[5] == 16:
+            print (self.Team_Data[2], 'clinched the game with 16 rounds won')
+
 
 # FUNCTIONS
 
 # MAIN
-game_finality = Finality(Game_Data, Team_Data)
+outcome = game_outcome(Game_Data, Team_Data)
 
-Game_Data[0] = 30
-Team_Data[4] = 15
-Team_Data[5] = 15
+Team_Data[4] = 16
+Team_Data[5] = 10
 
-game_finality.game_tie()
+outcome.game_tie() # declares a tie
+outcome.game_winner() # declares a winner
